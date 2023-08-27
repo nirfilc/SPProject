7a2b8ee8273a3565168e903f7061e583ed788f51
nirfilc <80191398+nirfilc@users.noreply.github.com>
2023-08-27 18:43:55 +0300

    add cors

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import PESrank
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/getPasswordStrength', methods=['POST'])
 def getPasswordStrength():
+    print("got call")
     data = request.get_json()
-
+    path = "C:\\Users\\nirfi\\Desktop\\text_files"
     if 'username' not in data or 'password' not in data:
         return jsonify({'error': 'Missing required properties'}), 400
 
     username = data['username']
     password = data['password']
     country = data.get('country', '')
-
-    res = PESrank.main(username, password, "C:\\Users\\nirfi\\Desktop\\text_files", country)
+    try:
+        res = PESrank.main(username, password, path, country)
+    except Exception as e:
+        print(e)
+        return jsonify({'error': 'Something went wrong'}), 500
 
 
     return jsonify({'message': 'Data received successfully',
