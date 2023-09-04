a6efb14233f80021fe080c14eae082e00d435ecd
Nir Filc <t-nirfilc@microsoft.com>
2023-09-04 19:38:57 +0300

    Fix country text files config

--- PESrank.py
+++ PESrank.py
@@ -132,7 +132,7 @@ def main(username, password, path, country="", ratio=500):
     return r, explain
 
 def get_country_rank(password, country, path, ratio):
-    path = os.path.join(path, country, "distributions")
+    path = os.path.join(path, country)
     return rank(password, path, country, 1, ratio)
 
 def get_lists(country):
