00ba3a1304d9c27d8532a821fe7961b5967999e1
Sara Khatib <skhatib@salesforce.com>
2023-09-05 23:43:47 +0300

    adding password strength and reason

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import math
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import PESrank
@@ -22,16 +23,72 @@ def getPasswordStrength():
     password = data['password']
     country = data.get('country', '')
     try:
-        r, explain, isCountryDistribution = PESrank.main(username, password, path, country)
+        res = PESrank.main(username, password, path, country)
     except Exception as e:
         print(e)
         return jsonify({'error': 'Something went wrong'}), 500
 
-
+    passwordStrength, explaination = prepareResult(res, country)
     return jsonify({'message': 'Data received successfully',
                     'username': username,
-                    'result': (r, explain, isCountryDistribution)
+                    'strength': passwordStrength,
+                    'reason': explaination 
                     })
+
+
+
+def prepareResult(res, country):
+    [rank, explain, isCountryDistribution] = res
+    n = 905*(10**6)
+    ex = False
+
+    countiresSampleSize = {
+        "China": 6152365,
+        "France": 33090150,
+        "Germany": 47567442,
+        "India": 2226505,
+        "Italy": 17904278,
+        "Japan": 4482526,
+        "Poland": 9811562,
+        "United Kingdom (common practice)": 18289350
+    }
+
+    if isCountryDistribution:
+        n = countiresSampleSize[country]
+
+    passwordStrength = "" 
+    if rank<0:
+        passwordStrength = "Strong"
+    else:
+        if math.log2(rank)<=30:
+            passwordStrength = "Weak"
+            ex=True
+        elif math.log2(rank)<=50:
+            passwordStrength = "Sub-Optimal"
+            ex=True
+        else:
+            passwordStrength = "Strong"
+
+    if isCountryDistribution: 
+        explaination = "According to this study, based on " + n + " leaked passwords from your country: \n"
+    else:
+        explaination = "According to this study, based on 905 million leaked passwords: \n"
+
+    if ex == True:
+        explaination += "Your password is based on the leaked word: '" +str(explain[0][1])+ "' that was used by",int(float(explain[0][2])*n), "people\n"
+        for lst in explain[1:]:
+            if math.ceil(float(lst[1])*n)>=100:
+                if lst[0]==1:
+                    explaination += "It uses a prefix that was used by",math.ceil(float(lst[1])*n), "people\n"
+                if lst[0]==3:
+                    explaination += "It uses a suffix that was used by",math.ceil(float(lst[1])*n), "people\n"
+                if lst[0]==4:
+                    explaination += "It uses a capitaliation pattern that was used by",math.ceil(float(lst[1])*n), "people\n"
+                if lst[0]==5:
+                    explaination += "It uses a l33t pattern that was used by",math.ceil(float(lst[1])*n), "people\n"
+    
+    return {"PasswordStrength": passwordStrength, "Explaination": explaination}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
